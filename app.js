a82bf6e7aea50de05dea20ca0c2bc60325c1b365
Brian Otte <brianotte@byu725042wks.app.byu.edu>
2017-10-19 21:39:27 -0600

    working api

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var firstApp = angular.module('ronApp', []);
-firstApp.controller('ronController', function($scope, ronService){
-
+firstApp.controller('ronController', function($scope, ronService, $http){
+  var baseUrl = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
   $scope.test = 'Ron controller is working';
   $scope.getNewQuote = function(){
-    ronService.getQuote().then(function(response){
-      console.log(response.data[0]);
-      $scope.quote = response.data[0];
+    $http.get(baseUrl).success(function(data){
+      console.log(data[0]);
+      $scope.quote = data[0];
     })
   }
   ronService.getQuote().then(function(response){
